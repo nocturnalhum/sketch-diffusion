e87ea1f6cafc5b62114cd1573761575ff65447b4
nocturnal_hum <thanin.chan@gmail.com>
2023-07-12 10:09:14 -0400

    Test touch-action:none to prevent scroll move on draw

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   return (
-    <main className={`flex flex-col items-center min-h-screen bg-slate-300`}>
+    <main
+      className={`flex flex-col items-center max-h-screen bg-slate-300 overflow-y-hidden`}
+    >
       <h1 className='my-2'>Sketch-Pad</h1>
       <Toolbox />
-      <div className='shadow-lg p-3 rounded-lg bg-amber-100/30 drop-shadow-md'>
+      <div className='shadow-lg p-3 rounded-lg bg-amber-100/30 drop-shadow-md touch-none'>
         <Canvas />
       </div>
     </main>

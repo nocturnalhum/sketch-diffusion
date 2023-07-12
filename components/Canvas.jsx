b863f37e0125ee6d275fbe29f58890adb59fe03a
nocturnal_hum <thanin.chan@gmail.com>
2023-07-12 11:00:34 -0400

    Implement canvas to support both mouse and mobile touch drawing

--- components/Canvas.jsx
+++ components/Canvas.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import Toolbox from './Toolbox';
+import React, { useEffect, useRef, useState } from 'react';
 
-export default function Canvas() {
+export default function Touchcanvas() {
   const [isDrawing, setIsDrawing] = useState(false);
-  const canvasRef = useRef();
+  const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
   useEffect(() => {
@@ -28,6 +27,7 @@ export default function Canvas() {
       ctx.canvas.height = window.innerHeight * 0.9;
       ctx.canvas.style.width = `${window.innerWidth * 0.9}px`;
       ctx.canvas.style.height = `${window.innerHeight * 0.9}px`;
+      ctx.lineWidth = 10;
     };
     window.addEventListener('resize', resize);
     return () => window.removeEventListener('resize', resize);
@@ -37,7 +37,10 @@ export default function Canvas() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
-    const startDrawing = (e) => {
+    // ============================================================================
+    // =============<<< Touch Events >>>===========================================
+    // ============================================================================
+    const handleTouchStart = (e) => {
       const { touches } = e;
       const { pageX, pageY } = touches[0];
       const rect = canvas.getBoundingClientRect();
@@ -49,7 +52,7 @@ export default function Canvas() {
       setIsDrawing(true);
     };
 
-    const draw = (e) => {
+    const handleTouchMove = (e) => {
       if (!isDrawing) return;
 
       const { touches } = e;
@@ -62,61 +65,72 @@ export default function Canvas() {
       contextRef.current.stroke();
     };
 
-    const finishDrawing = () => {
+    const handleTouchEnd = () => {
       const drawing = canvasRef.current.toDataURL('image/png');
       localStorage.setItem('drawing', drawing);
       contextRef.current.closePath();
       setIsDrawing(false);
     };
 
-    const cancelDrawing = () => {
+    // ============================================================================
+    // =============<<< Mouse Events >>>===========================================
+    // ============================================================================
+    const handleMouseDown = (e) => {
+      const { clientX, clientY } = e;
+      const rect = canvas.getBoundingClientRect();
+      const x = clientX - rect.left;
+      const y = clientY - rect.top;
+
+      contextRef.current.beginPath();
+      contextRef.current.moveTo(x, y);
+      setIsDrawing(true);
+    };
+
+    const handleMouseMove = (e) => {
+      if (!isDrawing) return;
+      const { clientX, clientY } = e;
+      const rect = canvas.getBoundingClientRect();
+      const x = clientX - rect.left;
+      const y = clientY - rect.top;
+
+      contextRef.current.lineTo(x, y);
+      contextRef.current.stroke();
+    };
+
+    const handleMouseUp = () => {
+      const drawing = canvasRef.current.toDataURL('image/png');
+      localStorage.setItem('drawing', drawing);
+      contextRef.current.closePath();
       setIsDrawing(false);
     };
 
-    canvas.addEventListener('touchstart', startDrawing);
-    canvas.addEventListener('touchmove', draw);
-    canvas.addEventListener('touchend', finishDrawing);
-    canvas.addEventListener('touchcancel', cancelDrawing);
+    const handleMouseOut = () => {
+      if (isDrawing) {
+        const drawing = canvasRef.current.toDataURL('image/png');
+        localStorage.setItem('drawing', drawing);
+        contextRef.current.closePath();
+        setIsDrawing(false);
+      }
+    };
+
+    canvas.addEventListener('touchstart', handleTouchStart);
+    canvas.addEventListener('touchmove', handleTouchMove);
+    canvas.addEventListener('touchend', handleTouchEnd);
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mouseup', handleMouseUp);
+    canvas.addEventListener('mouseout', handleMouseOut);
+
     return () => {
-      canvas.removeEventListener('touchstart', startDrawing);
-      canvas.removeEventListener('touchmove', draw);
-      canvas.removeEventListener('touchend', finishDrawing);
-      canvas.removeEventListener('touchcancel', cancelDrawing);
+      canvas.removeEventListener('touchstart', handleTouchStart);
+      canvas.removeEventListener('touchmove', handleTouchMove);
+      canvas.removeEventListener('touchend', handleTouchEnd);
+      canvas.removeEventListener('mousedown', handleMouseDown);
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener('mouseup', handleMouseUp);
+      canvas.removeEventListener('mouseout', handleMouseOut);
     };
   }, [isDrawing]);
 
-  // const startDrawing = (e) => {
-  //   console.log('Start Drawing');
-  //   const { offsetX: x, offsetY: y } = e.nativeEvent;
-  //   contextRef.current.beginPath();
-  //   contextRef.current.moveTo(x, y);
-  //   setIsDrawing(true);
-  // };
-
-  // const draw = (e) => {
-  //   if (!isDrawing) return;
-  //   const { offsetX: x, offsetY: y } = e.nativeEvent;
-  //   contextRef.current.lineTo(x, y);
-  //   contextRef.current.stroke();
-  // };
-
-  // const finishDrawing = () => {
-  //   const drawing = canvasRef.current.toDataURL('image/png');
-  //   localStorage.setItem('drawing', drawing);
-  //   contextRef.current.closePath();
-  //   setIsDrawing(false);
-  // };
-
-  return (
-    <canvas
-      ref={canvasRef}
-      // onMouseDown={startDrawing}
-      // onMouseMove={draw}
-      // onMouseUp={finishtDrawing}
-      // onMouseOut={finishDrawing}
-      className='border border-black bg-white'
-    />
-  );
+  return <canvas ref={canvasRef} className='border border-black bg-white' />;
 }
-
-// STOP HERE ....................................
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import { useEffect, useRef, useState } from 'react';
 export default function Home() {
   return (
     <main
-      className={`flex flex-col items-center max-h-screen bg-slate-300 overflow-y-hidden`}
+      className={`flex flex-col items-center min-h-screen bg-slate-300 touch-none`}
     >
       <h1 className='my-2'>Sketch-Pad</h1>
       <Toolbox />
-      <div className='shadow-lg p-3 rounded-lg bg-amber-100/30 drop-shadow-md touch-none'>
+      <div className='shadow-lg p-3 rounded-lg bg-amber-100/30 drop-shadow-md'>
         <Canvas />
       </div>
     </main>

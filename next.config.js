d49581ddf339cec3b2dc22165ec22927ec0d5930
nocturnal_hum <thanin.chan@gmail.com>
2023-07-18 16:56:19 -0400

    Update Replicate API Key and Gateway Timeout issue fix

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'https://controlnet-upload.s3.ca-central-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'sketch-canvas-images.s3.ca-central-1.amazonaws.com',
       },
     ],
